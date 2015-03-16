b2a16237b3b56a8752216522656020da9fd768ec
ivanc <ivanc@ethicalsoftware.net>
2015-03-16 08:29:38 +0100

    added trend

--- php/bztrafficwrapper.php
+++ php/bztrafficwrapper.php
@@ -1,35 +1,58 @@
 <?php
 class BZTrafficWrapper{
+
+    protected $cStation= NULL;
+    protected $cDataType= array();
+    protected $cStationDetail= NULL;
+    protected $cRecord= array();
+    protected $cRecordInTimeFrame= array();
+    protected $cDateOfLastRecord= array();
+
+    
     const URL='http://ipchannels.integreen-life.bz.it/LinkFrontEnd/rest';
 
     function get_stations(){
-        return file_get_contents( (self::URL)."/get-stations" );
+        if( $this->cStation == NULL ){
+            $this->cStation = file_get_contents( (self::URL)."/get-stations" );
+        }
+        return $this->cStation;
     }
 
     function get_data_types($id){
-        return file_get_contents( (self::URL)."/get-data-types?station=$id" );
+        if( !isset($this->cDataType[$id]) ){
+            $this->cDataType[$id] = file_get_contents( (self::URL)."/get-data-types?station=$id" );
+        }
+        return $this->cDataType[$id];
     }
 
-    function get_station_details($id=0){
-        if($id>0){
-            $url="/get-station-details?station=$id";
-        }else{
-            //$url="/get-station-details";
-            $url="/get-available-stations";
+    function get_station_details(){
+        if( $this->cStationDetail == NULL ) {
+            $this->cStationDetail= file_get_contents( (self::URL)."/get-available-stations" );
         }
-        return file_get_contents( (self::URL).$url );
+        return $this->cStationDetail;
     }
 
     function get_records($id, $data_type, $seconds = 3600){
-        return file_get_contents( (self::URL)."/get-records?station=$id&name=$data_type&seconds=$seconds" );
+        $key= "/get-records?station=$id&name=$data_type&seconds=$seconds";
+        if( !isset($this->cRecord[$key]) ){
+            $this->cRecord[$key] = file_get_contents( (self::URL).$key );
+        }
+        return $this->cRecord[$key];
     }
 
     function get_records_in_timeframe($id, $data_type, $start_date, $end_date){
-        return file_get_contents( (self::URL)."/get-records-in-timeframe?station=$id&name=$data_type&from=$start_date&to=$end_date" );
+        $key= "/get-records-in-timeframe?station=$id&name=$data_type&from=$start_date&to=$end_date";
+        if( !isset($this->cRecordInTimeFrame[$key]) ) {
+            $this->cRecordInTimeFrame[$key]= file_get_contents( (self::URL).$key );
+        }
+        return $this->cRecordInTimeFrame[$key];
     }
 
-    function get_date_of_last_record($id){
-        return file_get_contents( (self::URL)."/get-date-of-last-record?station=$id" );
+    function get_date_of_last_record($id) {
+        if( !isset($this->cDateOfLastRecord[$id]) ) {
+            $this->cDateOfLastRecord[$id]= file_get_contents( (self::URL)."/get-date-of-last-record?station=$id" );
+        }
+        return $this->cDateOfLastRecord[$id];
     }
 }
 ?>
